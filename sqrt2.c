6e90b16fe0c8a18fc2fc9460c3b6c573f4bf50b0
K-module <mathmutant@gmail.com>
2016-10-26 07:37:02 +0200

    Update sqrt2.c

--- sqrt2.c
+++ sqrt2.c
@@ -14,7 +14,7 @@ double sqrt2(double approx, int steps)
 
 int main(int argc, char **argv)
 {
-  if (argc < 2) {
+  if (argc < 3) {
   printf("%s <first guess> <steps>\n", argv[0]);
   return -1;
   }
