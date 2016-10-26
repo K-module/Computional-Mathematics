ee02c69b67a364b1b44f0fc9cb4e25ae9fef0827
K-module <mathmutant@gmail.com>
2016-10-26 07:34:38 +0200

    Update sqrt2.c

--- sqrt2.c
+++ sqrt2.c
@@ -1,25 +1,28 @@
+#A (finite) approximation of the infinite idea of the square root of 2.
+#By K-module
+
 #include <stdio.h>
 #include <stdlib.h>
 
 double sqrt2(double approx, int steps)
 {
-	for (int i=0; i<=steps; ++i) {
-		approx = .5*(approx + (2/approx)); 
-		printf("[ Step %d ] %.50f\n", i, approx);
-	}
+  for (int i=0; i<=steps; ++i) {
+  approx = .5*(approx + (2/approx)); 
+  printf("[ Step %d ] %.50f\n", i, approx);
+  }
 }
 
 int main(int argc, char **argv)
 {
-	if (argc < 2) {
-		printf("%s <first guess> <steps>\n", argv[0]);
-		return -1;
-	}
+  if (argc < 2) {
+  printf("%s <first guess> <steps>\n", argv[0]);
+  return -1;
+  }
 
-	double first 	= atof(argv[1]);
-	int steps			= atoi(argv[2]);
+  double first 	= atof(argv[1]);
+  int steps     = atoi(argv[2]);
 
-	sqrt2(first, steps);
+  sqrt2(first, steps);
 
-	return 0;
+  return 0;
 }
