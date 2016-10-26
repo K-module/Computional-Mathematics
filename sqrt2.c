58714d0f5c6f0f6ca550a39b8b846760aabca6bd
K-module <mathmutant@gmail.com>
2016-10-26 07:32:53 +0200

    Add files via upload

---
+++ sqrt2.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+double sqrt2(double approx, int steps)
+{
+	for (int i=0; i<=steps; ++i) {
+		approx = .5*(approx + (2/approx)); 
+		printf("[ Step %d ] %.50f\n", i, approx);
+	}
+}
+
+int main(int argc, char **argv)
+{
+	if (argc < 2) {
+		printf("%s <first guess> <steps>\n", argv[0]);
+		return -1;
+	}
+
+	double first 	= atof(argv[1]);
+	int steps			= atoi(argv[2]);
+
+	sqrt2(first, steps);
+
+	return 0;
+}
